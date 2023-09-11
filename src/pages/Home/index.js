19dd10c573071341dcdc7795a352bc57979a0fa9
Valene-R <valene.renaud@gmail.com>
2023-09-11 14:28:08 +0200

    fix: update the agency name to the new version and fix 'typos' in the code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -43,23 +43,23 @@ const Home = () => {
             Une soirée d’entreprise vous permet de réunir vos équipes pour un
             moment convivial afin de valoriser votre société en projetant une
             image dynamique. Nous vous proposons d’organiser pour vous vos
-            diners et soirée d’entreprise
+            diners et soirée d’entreprise.
           </ServiceCard>
           <ServiceCard imageSrc="/images/hall-expo.png">
             <h3>Conférences</h3>
-            77 events vous propose d’organiser votre évènement, quelle que soit
+            724 events vous propose d’organiser votre évènement, quelle que soit
             sa taille, en s’adaptant à votre demande et à vos demandes. En tant
             que spécialistes de l’évènementiel, nous saurons trouver le lieu
             parfait ainsi que des solutions inédites pour capter votre audience
-            et faire de cet évènement un succès
+            et faire de cet évènement un succès.
           </ServiceCard>
           <ServiceCard imageSrc="/images/sophia-sideri-LFXMtUuAKK8-unsplash1.png">
-            <h3>Experience digitale</h3>
+            <h3>Expérience digitale</h3>
             Notre agence experte en contenus immersifs offre des services de
             conseil aux entreprises, pour l’utilisation de la réalité virtuelle,
             de la réalité augmentée et de la réalité mixte de l’animation
             événementielle, à la veille technologique jusqu’au développement de
-            module de formation innovant
+            module de formation innovant.
           </ServiceCard>
         </div>
       </section>
@@ -110,8 +110,8 @@ const Home = () => {
             <div className={`ModalMessage--success ${successMessageVisible ? "visible" : ""}`}>
               <div>Message envoyé !</div>
               <p>
-                Merci pour votre message nous tâcherons de vous répondre dans
-                les plus brefs délais
+                Merci pour votre message. Nous tâcherons de vous répondre dans
+                les plus brefs délais.
               </p>
             </div>
           }
@@ -146,7 +146,7 @@ const Home = () => {
         <h3>Contactez-nous</h3>
         <address>45 avenue de la République, 75000 Paris</address>
         <div>01 23 45 67 89</div>
-        <div>contact@77events.com</div>
+        <div>contact@724events.com</div>
         <div>
           <a href="#twitch">
             <Icon name="twitch" />
@@ -168,7 +168,7 @@ const Home = () => {
           Une agence événementielle propose des prestations de service
           spécialisées dans la conception et l&apos;organisation de divers événements
           tels que des événements festifs, des manifestations sportives et
-          culturelles, des événements professionnels
+          culturelles, des événements professionnels.
         </p>
       </div>
     </footer>
